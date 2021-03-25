8d673c90a5537b46d2919438f7deb61b11685f49
arsalan-yousuf <arsalan98y@gmail.com>
2021-03-25 12:32:15 +0500

    Updated

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://arsalan:G1Qp5vAl02qth6zw@cluster0.8lad6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .connect('mongodb+srv://<password>@cluster0.8lad6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(() => {
     console.log('Database connected');
     console.log('Listening on localhost:5000')
